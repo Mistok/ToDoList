cb2b6d670a59381a9fe5d016c6ab01053166efd1
Mistok <Mistreated@ukr.net>
2020-04-19 18:43:41 +0300

    header component changet to stateless

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,25 +2,21 @@
 import React from 'react';
 import style from './header.scss';
 
-class Header extends React.Component {
-
-
-    render(){
-        return(<header>
-                <div className='container'>
-                    <div className={style.header_container}>
-                        <div className={style.logo_container}>
-                            <img className={style.logo} src="../../../assets/logo.png" alt="Logo"/>
-                        </div>
-                        <div className={style.menu}>
-                            <input className={style.search_field}  type="text" onClick={console}/>
-                            <img className={style.profile_ava}  src="../../../assets/ava.png" alt="ava"/>
-                        </div>
+const Header = () => {
+    return(<header>
+            <div className='container'>
+                <div className={style.header_container}>
+                    <div className={style.logo_container}>
+                        <img className={style.logo} src="../../../assets/logo.png" alt="Logo"/>
+                    </div>
+                    <div className={style.menu}>
+                        <input className={style.search_field}  type="text" onClick={console}/>
+                        <img className={style.profile_ava}  src="../../../assets/ava.png" alt="ava"/>
                     </div>
                 </div>
-            </header>
+            </div>
+        </header>
 
-        )
-    }
-}
+    )
+};
 export default Header;
