64cbd1a5fe934986312772c0011d279aef07b123
Mistok <Mistreated@ukr.net>
2020-05-17 12:49:21 +0300

    showMore function added

--- src/components/Content/Tasks/Tasks.jsx
+++ src/components/Content/Tasks/Tasks.jsx
@@ -9,11 +9,12 @@ import {showMore} from "../../../actions/actions";
 const FadeOutDown = styled.div`animation: 2s ${keyframes`${fadeOutDown}`} `;
 const Tasks = (props) => {
     //let [taskList, changeTasklist ]= useState(props.taskList); // локальный стейт, для взятия из LocalStorage
-    let [isShown, changeIsShown ] = useState(2); // локальная переменная, отображает сколько заданий показывать, по умолчанию 2
-    const showMore = () =>  isShown < props.taskList.length ? changeIsShown(isShown += 2): null; // показать еще заданий
+    let [shownCount, changeShownCount ] = useState(2); // локальная переменная, отображает сколько заданий показывать, по умолчанию 2
+    const showMore = () =>  shownCount < props.taskList.length ? changeShownCount(shownCount +=2): null; // показать еще заданий
 
     let shownTasks = [...props.taskList]; //
-    shownTasks.length = isShown;
+    shownTasks.length = shownCount;
+
 
     const secondSpanStyles = `${style.icon_right} + ${style.after}`; //Объеденяю стили
     return(
@@ -38,7 +39,7 @@ const Tasks = (props) => {
             }
             <button className={style.explore}
                     onClick={showMore}>
-                {isShown >= props.taskList.length ? 'No more tasks' : 'More tasks'}
+                {shownCount >= props.taskList.length ? 'No more tasks' : 'More tasks'}
 
                 <span className={style.icon_right}/>
                 <span className={style.secondSpanStyles}/>
